5c6f97bc9761bf599b271522c6176781ba396950
Jordan <jordan@enterpriseautomation.co.uk>
2024-06-28 13:45:15 +0100

    WORKING EXAMPLE
    code compiles and works

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,184 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:livespeechtotext/livespeechtotext.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Livespeechtotext _livespeechtotextPlugin;
+  late String _recognisedText;
+  String? _localeDisplayName = '';
+  StreamSubscription<dynamic>? onSuccessEvent;
+
+  bool microphoneGranted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _livespeechtotextPlugin = Livespeechtotext();
+
+    // _livespeechtotextPlugin.setLocale('ms-MY').then((value) async {
+    //   _localeDisplayName = await _livespeechtotextPlugin.getLocaleDisplayName();
+
+    //   setState(() {});
+    // });
+
+    _livespeechtotextPlugin.getLocaleDisplayName().then((value) => setState(
+          () => _localeDisplayName = value,
+        ));
+
+    // onSuccessEvent = _livespeechtotextPlugin.addEventListener('success', (text) {
+    //   setState(() {
+    //     _recognisedText = text ?? '';
+    //   });
+    // });
+
+    binding().whenComplete(() => null);
+
+    // _livespeechtotextPlugin
+    //     .getSupportedLocales()
+    //     .then((value) => value?.entries.forEach((element) {
+    //           print(element);
+    //         }));
+
+    _recognisedText = '';
+  }
+
+  @override
+  void dispose() {
+    onSuccessEvent?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Live Speech To Text'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(_recognisedText),
+              if (!microphoneGranted)
+                ElevatedButton(
+                  onPressed: () {
+                    binding();
+                  },
+                  child: const Text("Check Permissions"),
+                ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton(
+                      onPressed: microphoneGranted
+                          ? () {
+                              print("start button pressed");
+                              try {
+                                _livespeechtotextPlugin.start();
+                              } on PlatformException {
+                                print('error');
+                              }
+                            }
+                          : null,
+                      child: const Text('Start')),
+                  ElevatedButton(
+                      onPressed: microphoneGranted
+                          ? () {
+                              print("stop button pressed");
+                              try {
+                                _livespeechtotextPlugin.stop();
+                              } on PlatformException {
+                                print('error');
+                              }
+                            }
+                          : null,
+                      child: const Text('Stop')),
+                ],
+              ),
+              Text("Locale: $_localeDisplayName"),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<dynamic> binding() async {
+    onSuccessEvent?.cancel();
+
+    return Future.wait([]).then((_) async {
+      // Check if the user has already granted microphone permission.
+      var permissionStatus = await Permission.microphone.status;
+
+      // If the user has not granted permission, prompt them for it.
+      if (!microphoneGranted) {
+        await Permission.microphone.request();
+
+        // Check if the user has already granted the permission.
+        permissionStatus = await Permission.microphone.status;
+
+        if (!permissionStatus.isGranted) {
+          return Future.error('Microphone access denied');
+        }
+      }
+
+      // Check if the user has already granted speech permission.
+      if (Platform.isIOS) {
+        var speechStatus = await Permission.speech.status;
+
+        // If the user has not granted permission, prompt them for it.
+        if (!microphoneGranted) {
+          await Permission.speech.request();
+
+          // Check if the user has already granted the permission.
+          speechStatus = await Permission.speech.status;
+
+          if (!speechStatus.isGranted) {
+            return Future.error('Speech access denied');
+          }
+        }
+      }
+
+      return Future.value(true);
+    }).then((value) {
+      microphoneGranted = true;
+
+      // listen to event "success"
+      onSuccessEvent =
+          _livespeechtotextPlugin.addEventListener("success", (value) {
+        if (value.runtimeType != String) return;
+        if ((value as String).isEmpty) return;
+
+        setState(() {
+          _recognisedText = value;
+        });
+      });
+
+      setState(() {});
+    }).onError((error, stackTrace) {
+      // toast
+      log(error.toString());
+      // open app setting
+    });
   }
 }
